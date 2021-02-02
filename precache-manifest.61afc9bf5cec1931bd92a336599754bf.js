8f334c8014b7637d853ec87d72c5960336ad5716
43361_hrishikesh <hrishidalimbkar@gmail.com>
2021-02-02 09:18:55 +0530

    Updates

---
+++ precache-manifest.61afc9bf5cec1931bd92a336599754bf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adfca8f93df64a12e1df60e8677665c",
+    "url": "/reactwebapp/index.html"
+  },
+  {
+    "revision": "cb7bd3f7bf14631d295a",
+    "url": "/reactwebapp/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "ff5a68514b120b97912d",
+    "url": "/reactwebapp/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "cb7bd3f7bf14631d295a",
+    "url": "/reactwebapp/static/js/2.717f1fdb.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebapp/static/js/2.717f1fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5a68514b120b97912d",
+    "url": "/reactwebapp/static/js/main.f5ffb6f1.chunk.js"
+  },
+  {
+    "revision": "57f04e28da439dd1ec81",
+    "url": "/reactwebapp/static/js/runtime-main.1c32f715.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactwebapp/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactwebapp/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactwebapp/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactwebapp/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactwebapp/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactwebapp/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactwebapp/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactwebapp/static/media/software.43049cae.jpeg"
+  }
+]);
